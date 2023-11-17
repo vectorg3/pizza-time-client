9cdf9f9c7c3bf602ec9d04c4fe0d947ed1d1cc78
vectorg3 <diglytail@gmail.com>
2023-11-17 09:59:02 +0300

    first commit

---
+++ scripts/form.js
@@ -0,0 +1,86 @@
+const btnClick = async () => {
+    document.getElementById('submit__button').disabled = true;
+    let url = 'http://localhost:4444/';
+
+    let name = document.getElementById('name').value;
+    let phone = document.getElementById('phone').value;
+    let address = document.getElementById('address').value;
+    let pizza = document.getElementById('pizza').value;
+
+    if (validate(name, phone, address)) {
+        let user = {
+            name: name,
+            phone: phone,
+            address: address,
+            pizza: pizza,
+        };
+
+        try {
+            let response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8',
+                },
+                body: JSON.stringify(user),
+            });
+            if (response.ok) {
+                document.getElementById('success__notification').style.display =
+                    'flex';
+                setTimeout(() => {
+                    window.open('orders.html', '_self');
+                }, 3000);
+            } else {
+                document.getElementById('fetch__error').style.display = 'flex';
+            }
+        } catch (error) {
+            document.getElementById('fetch__error').style.display = 'flex';
+            document.getElementById('submit__button').disabled = false;
+        }
+    } else {
+        document.getElementById('submit__button').disabled = false;
+        return;
+    }
+};
+
+const validate = (name, phone, address) => {
+    // let isCorrect = true;
+    // if (name.length <= 1) {
+    //     document.getElementById('name__warn').innerHTML =
+    //         'Длина имени должна быть минимум 5 символов';
+    //     document.getElementById('name__error').style.display = 'flex';
+    //     isCorrect = false;
+    // }
+    // let isPhoneNumber = new RegExp(
+    //     /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g
+    // );
+    // if (!isPhoneNumber.test(phone)) {
+    //     document.getElementById('phone__warn').innerHTML =
+    //         'Некорректный формат номера телефона';
+    //     document.getElementById('phone__error').style.display = 'flex';
+    //     isCorrect = false;
+    // }
+    // if (address.length <= 5) {
+    //     document.getElementById('address__warn').innerHTML =
+    //         'Длина адреса должна быть минимум 6 символов';
+    //     document.getElementById('address__error').style.display = 'flex';
+    //     isCorrect = false;
+    // }
+    // if (isCorrect) return true;
+    // return false;
+    let isPhoneNumber = new RegExp(/(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g);
+    if(name !== name.toUpperCase()) {
+        alert('Имя должно быть указано большими буквами');
+        return false;
+    }else if (address.length < 5){
+        alert('Длина адреса должна быть не меньше 5 символов');
+        return false;
+    }else if (!isPhoneNumber.test(phone)){
+        alert('Неверно указан номер телефона');
+        return false;
+    }
+    alert('Форма заполнена верно!')
+    
+};
+const closeWarning = (field) => {
+    document.getElementById(field).style.display = 'none';
+};
---
+++ scripts/orders.js
@@ -0,0 +1,58 @@
+const renderOrders = async () => {
+  try {
+    await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((orders) => {
+        let innerHTML = ``;
+        orders.forEach((order) => {
+          innerHTML += `<div class="orders__item">
+          <div class="orders__row">
+            <span style="font-weight: 700">Пицца</span>${order.pizza}
+          </div>
+          <div class="orders__row">
+            <span style="font-weight: 700">Заказчик</span>${order.name}
+          </div>
+          <div class="orders__row">
+            <span style="font-weight: 700">Номер</span>${order.phone}
+          </div>
+          <div class="orders__row">
+            <span style="font-weight: 700">Адрес</span>${order.address}
+          </div>
+          <button id="delete__button" onclick="deleteOrder('${order._id}')">
+            Удалить заказ
+          </button>
+        </div>`;
+        });
+        document.getElementById("orders__container").innerHTML = innerHTML;
+      });
+  } catch (error) {
+    alert('Ошибка запроса на сервер')
+  }
+};
+
+const deleteOrder = async (id) => {
+  try {
+    let response = await fetch(url + `${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    }); 
+    if (response.ok) {
+      await renderOrders();
+    } else {
+      alert("Ошибка запроса");
+    }
+  } catch (error) {
+    alert("Ошибка запроса " + error);
+  }
+};
+
+let url = "http://localhost:4444/";
+renderOrders();
+
