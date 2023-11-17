13d3ce3322bbd875b109f808bf3b9f014cee16ff
vectorg3 <diglytail@gmail.com>
2023-11-17 10:08:28 +0300

    validation fixed

--- scripts/form.js
+++ scripts/form.js
@@ -43,43 +43,30 @@ const btnClick = async () => {
 };
 
 const validate = (name, phone, address) => {
-    // let isCorrect = true;
-    // if (name.length <= 1) {
-    //     document.getElementById('name__warn').innerHTML =
-    //         'Длина имени должна быть минимум 5 символов';
-    //     document.getElementById('name__error').style.display = 'flex';
-    //     isCorrect = false;
-    // }
-    // let isPhoneNumber = new RegExp(
-    //     /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g
-    // );
-    // if (!isPhoneNumber.test(phone)) {
-    //     document.getElementById('phone__warn').innerHTML =
-    //         'Некорректный формат номера телефона';
-    //     document.getElementById('phone__error').style.display = 'flex';
-    //     isCorrect = false;
-    // }
-    // if (address.length <= 5) {
-    //     document.getElementById('address__warn').innerHTML =
-    //         'Длина адреса должна быть минимум 6 символов';
-    //     document.getElementById('address__error').style.display = 'flex';
-    //     isCorrect = false;
-    // }
-    // if (isCorrect) return true;
-    // return false;
-    let isPhoneNumber = new RegExp(/(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g);
-    if(name !== name.toUpperCase()) {
-        alert('Имя должно быть указано большими буквами');
-        return false;
-    }else if (address.length < 5){
-        alert('Длина адреса должна быть не меньше 5 символов');
-        return false;
-    }else if (!isPhoneNumber.test(phone)){
-        alert('Неверно указан номер телефона');
-        return false;
+    let isCorrect = true;
+    if (name.length <= 1) {
+        document.getElementById('name__warn').innerHTML =
+            'Длина имени должна быть минимум 5 символов';
+        document.getElementById('name__error').style.display = 'flex';
+        isCorrect = false;
     }
-    alert('Форма заполнена верно!')
-    
+    let isPhoneNumber = new RegExp(
+        /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g
+    );
+    if (!isPhoneNumber.test(phone)) {
+        document.getElementById('phone__warn').innerHTML =
+            'Некорректный формат номера телефона';
+        document.getElementById('phone__error').style.display = 'flex';
+        isCorrect = false;
+    }
+    if (address.length <= 5) {
+        document.getElementById('address__warn').innerHTML =
+            'Длина адреса должна быть минимум 6 символов';
+        document.getElementById('address__error').style.display = 'flex';
+        isCorrect = false;
+    }
+    if (isCorrect) return true;
+    return false;
 };
 const closeWarning = (field) => {
     document.getElementById(field).style.display = 'none';
